14853e5e3382dca19557f63f3299e5b0f2db7af5
Shankar Sai Ganesh <shankarsaiganesh@gmail.com>
2022-09-30 20:13:20 +0800

    first commit

---
+++ src/test/java/ui/AfterBeforeSuiteTest.java
@@ -0,0 +1,54 @@
+package ui;
+
+import common.CommonDataSetup;
+import org.testng.annotations.*;
+
+// notice that test cases without priority is run first before priority = 1 and 2. So better to set priority for everything or for nothing.
+// Or if a test case needs to be run before a specific test case.
+
+
+@Test(groups = "user-registration")
+public class AfterBeforeSuiteTest extends CommonDataSetup {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("executed before all tests in this class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("executed after all tests in this class..");
+    }
+
+    @BeforeGroups(value = "regression")
+    public void beforeRegressionGroup(){
+        System.out.println("Executed before regression group");
+    }
+
+    @AfterGroups(value = {"regression","bvt"}) // after each test in the group; only if run configuration is for group not if run class;
+    public void afterRegressionBVTGroups(){
+        System.out.println("executed after regression and bvt");
+    }
+
+    @Test(priority = 1, groups="regression")
+    public void Test1() {
+        System.out.println("Test1 executed.");
+    }
+
+
+    @Test (priority = 2, groups="regression")
+    public void Test2() {
+        System.out.println("Test2 executed.");
+    }
+
+    @Test (groups={"regression", "bvt"})
+    public void Test3() {
+        System.out.println("Test3 executed.");
+    }
+
+    @Test ( groups = "bvt")
+    public void Test4() {
+        System.out.println("Test4 executed.");
+    }
+
+}
---
+++ src/test/java/ui/VerifyTitleAndTextTest.java
@@ -0,0 +1,26 @@
+package ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyTitleandTextTest {
+
+    @Test
+    public void titleTest() throws InterruptedException
+    {
+        String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
+        String expectedText = "Search";
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+
+        driver.close();
+    }
+
+}
