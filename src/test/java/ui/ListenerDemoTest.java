bb27fe03861eca009be745367c33e5370c332e42
Shankar Sai Ganesh <shankarsaiganesh@gmail.com>
2022-10-01 12:01:25 +0800

    update listener

--- src/test/java/ui/ListenerDemoTest.java
+++ src/test/java/ui/ListenerDemoTest.java
@@ -1,11 +1,13 @@
 package ui;
 
 import common.BaseTest;
+import common.Retry;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ListenerDemoTest extends BaseTest {
 
+//    @Test(retryAnalyzer = Retry.class) // since listener can be added to xml file
     @Test
      public void launchApp()
      {
