d5b178e8d473a9aa3987afde8313b2b36eae38df
Shankar Sai Ganesh <shankarsaiganesh@gmail.com>
2022-10-04 19:23:15 +0800

    remove comments

--- src/test/java/common/Listeners.java
+++ src/test/java/common/Listeners.java
@@ -33,29 +33,23 @@ public class Listeners extends testUtils implements ITestListener{
         }
         
         System.setProperty("org.uncommons.reportng.escape-output", "false"); // this is to view the screenshot below. HTML characters will be recognised
-        // hard-coded screenshot here
         
 		Reporter.log("<a href=\"C:\\Users\\shank\\eclipse-workspace\\seleniumTesting\\screenshot\\"+ screenshotfilename +"\">Test Results</a>");
     }
 
     public void onTestSkipped(ITestResult result) {
-        // TODO Auto-generated method stub
     }
 
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        // TODO Auto-generated method stub
     }
 
     public void onTestFailedWithTimeout(ITestResult result) {
-        // TODO Auto-generated method stub
     }
 
     public void onStart(ITestContext context) {
-        // TODO Auto-generated method stub
     }
 
     public void onFinish(ITestContext context) {
-        // TODO Auto-generated method stub
     }
 
 }
